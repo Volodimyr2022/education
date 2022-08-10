f9a5a99b46a1d01471daafb07d0730561e155b04
Vladimir <107769314+Volodimyr2022@users.noreply.github.com>
2022-08-10 11:52:10 +0300

    Add files via upload

--- Practice/scpipt/script.js
+++ Practice/scpipt/script.js
@@ -5,7 +5,8 @@ $('.hiring_items > div').on('click', function(){
 })
 $('.find_items > div').on('click', function(){
    $(this).children('.find_pretitle-text').toggleClass('change_color-find')
-   $(this).children('.find_img').toggleClass('change_color-find')
+   // $(this).children('.find_img').toggleClass('change_color-find')
     $(this).parent('.find_items').toggleClass('change_color-find')
+    $(this).children('.find_img').children('svg').children('path').toggleClass('change_color-icon')
   $(this).next('.find_block').slideToggle(300)
 })
