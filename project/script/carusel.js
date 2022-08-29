8a95f591c4ecfc54a46e98168abeb8e9c6987b55
Vladimir <107769314+Volodimyr2022@users.noreply.github.com>
2022-08-29 19:12:34 +0300

    Add files via upload

--- project/script/carusel.js
+++ project/script/carusel.js
@@ -1,6 +1,21 @@
-$('.carusel').slick({
+$('.carusel-space').slick({
+    Infinity: false,
+    slidesToShow: 3,
+    dots: true,
+    arrows: false,
+})
+$('.carusel-gallery').slick({
     Infinity: false,
     prevArrow: $('.prev'),
-    nextArrow: $('.next'),  
+    nextArrow: $('.next'),
+    slidesToShow: 2,
+    dots: false,
+})
+$('.carusel-reviews').slick({
+    Infinity: false,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    slidesToShow: 2,
     dots: true,
 })
+
